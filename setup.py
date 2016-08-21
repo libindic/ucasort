5cca6f1cd51dfd6a4d2a3839c9b8c730243e7b84
Jerin Philip <monu1618@gmail.com>
2016-08-21 11:26:38 +0530

    WSME REST Compatibility
    setup.py, setup.cfg -> pbr enabled. entrypoints added.
    ucasort/expose.py -> Aux functions for WSME REST.
    ucasort/__init__.py -> Relative import fix

--- setup.py
+++ setup.py
@@ -1,21 +1,8 @@
 #!/usr/bin/env python
-
-from setuptools import setup, find_packages
-
-name = "ucasort"
+from setuptools import setup
 
 setup(
-    name = name,
-    version = "0.1.1",
-    url = "http://silpa.org.in/sort",
-    license = "LGPL-3.0",
-    description = "Sort words basded on linguistics",
-    author = "Santhosh Thottingal",
-    author_email = "santhosh.thottingal@gmail.com",
-    long_description = """Sort words basded on linguistics""",
-    packages = find_packages(),
-    include_package_data = True,
-    setup_requires = ['setuptools-git'],
-    install_requires = ['setuptools', 'pyuca'],
-    zip_safe = False,
-    )
+    setup_requires=['pbr'],
+    pbr=True
+)
+
